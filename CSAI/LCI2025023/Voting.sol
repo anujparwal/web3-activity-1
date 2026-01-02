4465a78e65696e5fb2ca22a0caabf6a650d38c49
Anuj Parwal <anujparwal7787@gmail.com>
2026-01-02 17:10:00 +0530

    Update Voting.sol

--- CSAI/LCI2025023/Voting.sol
+++ CSAI/LCI2025023/Voting.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-contract Betting{
+contract Voting{
     struct Candidate{
         string Candidate_name;
         address Candidate_address;
